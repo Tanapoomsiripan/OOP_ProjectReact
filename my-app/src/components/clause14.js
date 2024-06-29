b3347b02e9a7a18307c766dd0ebd57b0ea449115
Tanapoom <s6304062616111@email.kmutnb.ac.th>
2024-06-29 13:46:56 +0700

    first commit

---
+++ my-app/src/components/clause14.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Clause14(){
+
+
+    return(
+        <div>
+            <input></input><input></input>
+            
+        </div>
+    )
+}
+export default Clause14;
